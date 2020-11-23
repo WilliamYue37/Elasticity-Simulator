88ebf1a65fb30bf70538e08566ee1f2b034a81f4
Victor Wang <victorwang37@gmail.com>
2020-11-23 15:27:49 -0600

    moving line

--- simulation.py
+++ simulation.py
@@ -1,32 +1,17 @@
-from tkinter import Tk, Canvas, Frame, BOTH
+from tkinter import *
+import time
 
-class Simulation(Frame):
+mGui = Tk()
+mGui.title('GUI Example')
+mGui.geometry('1024x576+10+30')
 
-    def __init__(self):
-        super().__init__()
+canvas_1 = Canvas(mGui,height=500,width=500,bg='grey')
 
-        self.initUI()
+canvas_1.pack()
 
+for i in range(1, 500):
+    time.sleep(0.5)
+    canvas_1.create_line(0,0,i,i)
+    canvas_1.update()
 
-    def initUI(self):
-
-        self.master.title("Elasticity Simulation")
-        self.pack(fill=BOTH, expand=1)
-
-        canvas = Canvas(self)
-        canvas.create_oval(350, 50, 450, 150, outline="#000000", fill="#000000", width=2)
-        canvas.create_rectangle(0, 600, 800, 800, outline="#000000", fill="#75381a", width=2)
-
-        canvas.pack(fill=BOTH, expand=1)
-
-
-def main():
-
-    root = Tk()
-    ex = Simulation()
-    root.geometry("800x800+100+100")
-    root.mainloop()
-
-
-if __name__ == '__main__':
-    main()
+mGui.mainloop()

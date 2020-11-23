4435615f47abc8f8f814a9350500ce26a49cd599
Victor Wang <victorwang37@gmail.com>
2020-11-23 16:02:33 -0600

    drop fix

--- simulation.py
+++ simulation.py
@@ -9,7 +9,7 @@ canvas = Canvas(mGui,height=1000,width=1000,bg='white')
 canvas.create_rectangle(0, 800, 1000, 1000, outline="#000000", fill="#75381a", width=2)
 canvas.pack()
 
-canvas_1.pack()
+canvas.pack()
 
 preID = None
 numBounces = 0
@@ -24,8 +24,11 @@ while elapsed < 50:
         height = dropHeight + g * elapsed**2 / 2
     # else:
 
-    if preID != None: canvas_1.delete(preID)
-    preID = canvas_1.create_oval(100, height - radius, 100 + radius * 2, height + radius)
-    canvas_1.update()
+    if preID != None: canvas.delete(preID)
+    preID = canvas.create_oval(100, height - radius, 100 + radius * 2, height + radius)
+    canvas.update()
+
+    elapsed += 0.1
+    time.sleep(0.1)
 
 mGui.mainloop()

cb84276a04dcb2bef75c01f6fe7cc436ba59890a
William Yue <williamyue37@gmail.com>
2020-11-23 15:36:02 -0600

    changed variable name of ID

--- simulation.py
+++ simulation.py
@@ -9,12 +9,12 @@ canvas_1 = Canvas(mGui,height=500,width=500,bg='grey')
 
 canvas_1.pack()
 
-pre = None
+preID = None
 for i in range(1, 500):
     time.sleep(0.5)
-    if pre != None:
-        canvas_1.delete(pre)
-    pre = canvas_1.create_oval(i,i,i+100,i+100)
+    if preID != None:
+        canvas_1.delete(preID)
+    preID = canvas_1.create_oval(i,i,i+100,i+100)
     canvas_1.update()
 
 mGui.mainloop()

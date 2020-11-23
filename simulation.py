cae954e6398e9d6c4360511738cd07e2d9a645c3
Victor Wang <victorwang37@gmail.com>
2020-11-23 15:52:05 -0600

    drop

--- simulation.py
+++ simulation.py
@@ -10,11 +10,23 @@ canvas_1 = Canvas(mGui,height=500,width=500,bg='grey')
 canvas_1.pack()
 
 preID = None
-for i in range(1, 500):
-    time.sleep(0.5)
-    if preID != None:
-        canvas_1.delete(preID)
-    preID = canvas_1.create_oval(i,i,i+100,i+100)
+numBounces = 0
+dropHeight = 0
+height = dropHeight
+velocity = 0
+radius = 50
+g = 9.8
+elapsed = 0
+while elapsed < 50:
+    if numBounces == 0:
+        height = dropHeight + g * elapsed**2 / 2
+    # else:
+
+    if preID != None: canvas_1.delete(preID)
+    preID = canvas_1.create_oval(100, height - radius, 100 + radius * 2, height + radius)
     canvas_1.update()
+
+    elapsed += 0.1
+    time.sleep(0.1)
 
 mGui.mainloop()

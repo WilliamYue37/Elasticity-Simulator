91d8f042d6efc55fa49298a45c2df33ea509b338
Victor Wang <victorwang37@gmail.com>
2020-11-23 17:05:43 -0600

    drop update

--- simulation.py
+++ simulation.py
@@ -29,15 +29,16 @@ while totalTime < 1000:
         height = radius + v_after * elapsed - 0.5 * g * elapsed**2
 
     if preID != None: canvas.delete(preID)
+    height = max(height, radius)
     preID = canvas.create_oval(100, (floor - height) - radius, 100 + radius * 2, (floor - height) + radius)
     canvas.update()
 
-    if height < radius:
+    if height == radius:
         numBounces += 1
         lastDrop = totalTime
         v_after *= sqrt(elasticity)
-    totalTime += 0.1
-    time.sleep(0.01)
+    totalTime += 0.5
+    time.sleep(0.05)
 
 
 mGui.mainloop()

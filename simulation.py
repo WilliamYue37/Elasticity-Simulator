b1ad3ebc7790d06305f956112100b645cec58084
William Yue <williamyue37@gmail.com>
2020-11-23 15:33:25 -0600

    moving circle

--- simulation.py
+++ simulation.py
@@ -9,9 +9,12 @@ canvas_1 = Canvas(mGui,height=500,width=500,bg='grey')
 
 canvas_1.pack()
 
+pre = None
 for i in range(1, 500):
     time.sleep(0.5)
-    canvas_1.create_line(0,0,i,i)
+    if pre != None:
+        canvas_1.delete(pre)
+    pre = canvas_1.create_oval(i,i,i+100,i+100)
     canvas_1.update()
 
 mGui.mainloop()

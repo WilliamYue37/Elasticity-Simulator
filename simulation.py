96a68ef4f86bfe12b702e5d096722f6afae2d6c3
William Yue <williamyue37@gmail.com>
2020-12-03 20:21:29 -0600

    commented the code

--- simulation.py
+++ simulation.py
@@ -2,14 +2,17 @@ from tkinter import *
 import time
 from math import *
 
+# setup the GUI and specify the window size
 gui = Tk()
 gui.title('Elasticity Simulation')
 gui.geometry('1750x1000+10+10')
 
+# set the elevation of the floor and initiate the Canvas
 floor = 1000
 canvas = Canvas(gui, height=floor, width=1000, bg='white')
-canvas.grid(row=0, column=0, rowspan=1000)
+canvas.grid(row=0, column=0, rowspan=1000) # set up the grid layout - the rowspan of 1000 is a hack to eliminate spacing between table rows
 
+# parameter class for handing operations regarding the parameter table
 class Parameters:
     def draw(self):
         # Initial Parameters
@@ -71,26 +74,30 @@ class Parameters:
         self.root = root
         self.draw()
 
+    # used to read user input - return current parameters in the table cells
     def read(self):
         return float(self.mass.get()), float(self.elas.get()), float(self.height.get());
 
+    # lets user edit the table cells
     def enableTxt(self):
         self.mass.config(state=NORMAL)
         self.elas.config(state=NORMAL)
         self.height.config(state=NORMAL)
 
+    # prevents user from editing the table cells
     def disableTxt(self):
         self.mass.config(state=DISABLED)
         self.elas.config(state=DISABLED)
         self.height.config(state=DISABLED)
-param = Parameters(gui)
+param = Parameters(gui) # initiate the parameter table
 
 preID = None
 
+# variables to store the state of the buttons
 started = False
 paused = False
 var = IntVar()
-def startReset():
+def startReset(): # method to handle the clicking of the "start/reset" button
     global started
     global paused
     global table
@@ -110,7 +117,7 @@ def startReset():
         startResetButton.config(text="Start", bg="blue")
         pausePlayButton.config(text="Pause", bg="grey")
         param.enableTxt()
-def pausePlay():
+def pausePlay(): # method to handle the clicking of the "start/reset" button
     global paused
     global var
     if not paused:
@@ -123,13 +130,16 @@ def pausePlay():
         pausePlayButton.config(text="Pause", bg="grey")
         var.set(1)
 
+# initialize the 2 buttons onto the GUI canvas
 startResetButton = Button(text="Start", width=25, height=5, bg="blue", fg="white", font=('Candara', 11, 'bold'), command = startReset)
 pausePlayButton = Button(text="Pause", width=25, height=5, bg="grey", fg="white", font=('Candara', 11, 'bold'), command = pausePlay)
 
 startResetButton.grid(row=925, column=1) #Start/reset
 pausePlayButton.grid(row=925, column=2) #Pause/Play
 
+# class for the table containg values like momentum, kinetic energy, etc.
 class Table:
+	# method to update the table cells based on the values in the matrix "lst"
     def draw(self):
         for i in range(len(self.lst)): 
             for j in range(len(self.lst[i])):   
@@ -157,13 +167,17 @@ class Table:
                 self.mat[i][j].config(state=DISABLED)
         self.draw()
 
+    # update the class matrix "lst" with new values
     def update(self, *args):
         for i in range(len(self.lst)):
             self.lst[i][1] = args[0][i]
         self.draw()
 
-table = Table(gui)
-def run(mass, elasticity, dropHeight):
+table = Table(gui) # intialize the table
+
+# method for controlling the movement of the ball
+def run(mass, elasticity, dropHeight): # takes in initial parameters from user input through the parameter table
+    # initialize variables
     global table
     global preID
     numBounces = 0
@@ -174,30 +188,36 @@ def run(mass, elasticity, dropHeight):
     lastDrop = -1
     iterations = 0
 
+    # main loop to handle the movement of the ball at every instance of time
     while started:
-        if numBounces == 0:
+    	# compute the height
+        if numBounces == 0: # special case to handle inital drop
             height = dropHeight - 1/2 * g * totalTime**2
         else:
             elapsed = totalTime - lastDrop
             height = v_after * elapsed - 1/2 * g * elapsed**2
 
-        if preID != None: canvas.delete(preID)
-        height = max(height, 0)
+        if preID != None: canvas.delete(preID) # safety check
+        height = max(height, 0) # ensures our ball doesn't go into the ground
         preID = canvas.create_oval(100, (floor - height) - radius * 2, 100 + radius * 2, floor - height, fill="purple", outline="purple")
-        velocity = sqrt(max(v_after ** 2 - 2 * g * height, 0))
-        if velocity < 1e-3 and height == 0: velocity = 0
+        velocity = sqrt(max(v_after ** 2 - 2 * g * height, 0)) # compute the velocity
+        if velocity < 1e-3 and height == 0: velocity = 0 # manually set the velocity to zero when the velocity becomes to small to see movement on the screen
+        # compute values to update the table
         KE = 1/2 * mass * velocity**2
         PE = mass * g * height
         if iterations % 2 == 0 or velocity == 0 and height == 0: table.update([totalTime, height, numBounces, velocity, mass * velocity, KE, PE, KE + PE])
-        if velocity == 0 and height < 1e-3: break
+        if velocity == 0 and height < 1e-3: break # exit the loop when the movement of the ball is too neglible to been seen on the screen.
         canvas.update()
 
+        # handle the change of velocity after the collision
         if height == 0:
             numBounces += 1
             lastDrop = totalTime
             v_after *= sqrt(elasticity)
+
+        # update time and iteration variables
         totalTime += 0.5
-        time.sleep(0.05)
+        time.sleep(0.05) # slows the refresh rate so that machines with less powerful graphics won't see a deformation/glitch in the ball
         iterations += 1
 
 gui.mainloop()
